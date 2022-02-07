009550e5350c85f84c2b9350b3f0dad31fa033d2
markosbahgat <markos.bahgat@gmail.com>
2022-02-07 22:18:57 +0200

    Final Touches

--- src/slices/movieSlice.js
+++ src/slices/movieSlice.js
@@ -1,1 +1,18 @@
+/*
 
+import { createSlice } from "@reduxjs/toolkit";
+
+const movieSlice = createSlice({
+    name:"movie",
+    initialState : {},
+    reducers:{
+        postMovie: (state, action) => { console.log(state, action) },
+        updateMovie: (state, action) => { console.log(state, action) },
+        deleteMovie: (state, action) => { console.log(state, action) },
+    },
+    extraReducers:{
+        //here we will add some reducer that will listen to another reducers or to listen to thunk middelware while fetching from api
+    }
+})
+
+*/
